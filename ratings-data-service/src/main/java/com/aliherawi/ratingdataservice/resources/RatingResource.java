96b3f556423ba9bb36751f276275b1d9cd535057
Aliherawi <ALiherawi7@gmail.com>
2022-06-21 16:22:50 +0430

    the main RequesMapping address change to (ratingdata)

--- ratings-data-service/src/main/java/com/aliherawi/ratingdataservice/resources/RatingResource.java
+++ ratings-data-service/src/main/java/com/aliherawi/ratingdataservice/resources/RatingResource.java
@@ -2,31 +2,27 @@ package com.aliherawi.ratingdataservice.resources;
 
 import com.aliherawi.ratingdataservice.models.Rating;
 import com.aliherawi.ratingdataservice.models.UserRating;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/ratingdata")
 public class RatingResource {
+    @Autowired
+    private RatingService ratingService;
 
-    @RequestMapping("/{movieId}")
-    public Rating getMovie(@PathVariable int movieId) {
-        return new Rating(movieId, 4);
+    @RequestMapping("/{userId}")
+    public UserRating getRating(@PathVariable String userId) {
+        return ratingService.getRatingsByUserId(userId);
     }
 
-    @RequestMapping("users/{userId}")
-    public UserRating getRating() {
-        List<Rating> ratings = Arrays.asList(
-                new Rating(1, 4),
-                new Rating(2, 5)
-        );
-        UserRating userRating = new UserRating();
-        userRating.setUserRating(ratings);
-        return userRating;
+    @RequestMapping(method= RequestMethod.POST)
+    public void addRating(@RequestBody Rating rating){
+        ratingService.addRating(rating);
     }
+
 
 }
